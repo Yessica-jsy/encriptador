88a8ec12305112a1f873488d058c5939d243aca3
Yessica Rueda <yrledesma9@gmail.com>
2024-07-22 20:17:12 -0500

    cambio de imagen y placeHolder, ademas se agrego nuevas funciones

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
+let foto = document.getElementById('puppy')
 let text = document.getElementById("texto");
 let encrit = document.getElementById("encriptar");
 let desencrit = document.getElementById("desencriptar");
 let result = document.getElementById("resultado");
 let copy = document.getElementById("copiar");
 
-/*text.addEventListener("click", function () {
+text.addEventListener("click", function () {
   text.value = "";
   return;
-  break;
-});*/
+});
 //funcion
+function initialize() {
+  assignText(
+    "texto","Ingresar el testo que se desea desencriptar o encriptar");
+  assignText("resultado", "No se ha encontrado el texto"); 
+  document.getElementById('puppy').removeAttribute('hidden')
+}
+
+function assignText(element, text) {
+  let elementHtml = document.getElementById(element); // Select the HTML element
+  elementHtml.value = text; // Assign the text to the element
+}
 encrit.addEventListener("click", function () {
   const inputext = text.value;
-  const remplasar = inputext.replace(/e/gi, "enter").replace(/a/gi, "ai").replace(/i/gi, "imes").replace(/o/gi, "ober").replace(/u/gi, "uful");
+  if (inputext == "") {
+    initialize();
+    return;
+  }
+  const remplasar = inputext
+  .replace(/e/gi, "enter")
+  .replace(/i/gi, "imes")
+  .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "uful");
   result.value = remplasar;
+  document.getElementById('puppy').setAttribute('hidden', 'true')
   return;
 });
 desencrit.addEventListener("click", function () {
   const inputext = text.value;
-  const reemplasar = inputext.replace(/uful/gi, "u").replace(/ober/gi, "o").replace(/imes/gi, "i").replace(/ai/gi, "a").replace(/enter/gi, "e");
+  if (inputext == "") {
+    initialize();
+    return;
+  }
+  const reemplasar = inputext
+  .replace(/uful/gi, "u")
+  .replace(/ober/gi, "o")
+  .replace(/imes/gi, "i")
+  .replace(/ai/gi, "a")
+  .replace(/enter/gi, "e");
   result.value = reemplasar;
+  document.getElementById('puppy').setAttribute('hidden', 'true')
   return;
 });
 copy.addEventListener("click", function () {
-  navigator.clipboard.writeText(result.value)
+  navigator.clipboard
+  .writeText(result.value)
   .then(() => {
-    console.log('Texto copiado al portapapeles');
+    alert("Texto copiado al portapapeles");
   })
-  .catch(err => {
-    console.error('Error al copiar el texto: ', err);
+  .catch((err) => {
+    console.error("Error al copiar el texto: ", err);
   });
-  return;
-});
+    return;
+  });
+  //prueba
+
+  
+
+
+initialize();
